5bc1257ade3223d4a1f87bfb3dbcfca785799531
Michael McCarthy <michael.mccarthy@michael-mccarthy.biz>
2023-01-22 18:28:30 -0500

    first commit

---
+++ src/main/java/com/tech/ash/abuseipdb/AbuseIpdbServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tech.ash.abuseipdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbuseIpdbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbuseIpdbServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech/ash/abuseipdb/AbuseIpdbServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.ash.abuseipdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbuseIpdbServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
